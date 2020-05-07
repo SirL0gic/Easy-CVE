b4ad532db1c83ae65ae04c4ab81e8b8b4da34650
SirL0gic <63680552+SirL0gic@users.noreply.github.com>
2020-05-07 14:09:48 -0400

    New updaates

--- easy-cve.sh
+++ easy-cve.sh
@@ -3,68 +3,77 @@ echo "$(figlet EASY-CVE)" "By ABIS"
 echo "Twiiter: SirL0gic"
 echo
 echo
-echo "1)Simple Scan 2)Heavy Scan 3)Light Vulnerablity Scan 4)Heavy Vulnerablity Scan"
+echo "1)Light Vulnerablity Scan"
+echo "2)Heavy Vulnerablity Scan"
+echo 
 read -p "Choose an option:" option 
+echo
 echo
 
 
+        
 if [ $option = "1" ]
     then
-    read -p  "Enter IP: " ip
-    echo "$(nmap $ip)" 
-
-elif [ $option = "2" ]
-    then
-    read -p  "Enter IP: " ip
-    read -p "Port? y/n: " portoption
+    read -p  "[+]Enter IP: " ip
+    read -p "[+]Port? y/n: " portoption
 
     if [ $portoption = "y" ]
         then
-        read -p  "Enter Port: " port
-        echo "$(nmap -A $ip -p $port)" 
-    else
-        echo "$(nmap -A $ip)" 
-        fi
-        
-        
-elif [ $option = "3" ]
-    then
-    read -p  "Enter IP: " ip
-    echo "$(nmap  -Pn --script=vulners.nse $ip)" 
-    
-
-elif [ $option = "4" ]
-    then
-    read -p  "Enter IP: " ip
-    read -p "Port? y/n: " portoption
-
-    if [ $portoption = "y" ]
-        then
-        read -p  "Enter Port: " port
-        read -p "Save results? y/n: " save
+        read -p  "[+]Enter Port: " port
+        read -p "[+]Save results? y/n: " save
         
              if [ $save = "y" ]
                 then
-                read -p  "Location: " savelocation
-                echo "$(nmap -oN $savelocation -sV --script=vulscan/vulscan.nse $ip -p $port)" 
+                read -p  "[+]Location: " savelocation
+                echo "$(nmap -oN $savelocation -sV -A --script=vulners.nse $ip -p $port)" 
             else
-                echo "$(nmap -sV --script=vulscan/vulscan.nse $ip -p $port)" 
+                echo "$(nmap -sV -A --script=vulners.nse $ip -p $port)" 
                 fi
             
        
     else
-            read -p "Save results? y/n: " save
+            read -p "[+]Save results? y/n: " save
             if [ $save = "y" ]
                     then
-                    read -p  "Location: " savelocation
-                    echo "$(nmap -oN $savelocation -sV --script=vulscan/vulscan.nse $ip)" 
+                    read -p  "[+]Location: " savelocation
+                    echo "$(nmap -oN $savelocation -sV -A --script=vulners.nse $ip)" 
                 else
-                    echo "$(nmap -sV --script=vulscan/vulscan.nse $ip)" 
+                    echo "$(nmap -sV -A --script=vulners.nse $ip)" 
+                    fi
+fi
+    
+
+elif [ $option = "2" ]
+    then
+    read -p  "[+]Enter IP: " ip
+    read -p "[+]Port? y/n: " portoption
+
+    if [ $portoption = "y" ]
+        then
+        read -p  "[+]Enter Port: " port
+        read -p "[+]Save results? y/n: " save
+        
+             if [ $save = "y" ]
+                then
+                read -p  "[+]Location: " savelocation
+                echo "$(nmap -oN $savelocation -sV -A --script=vulscan/vulscan.nse $ip -p $port)" 
+            else
+                echo "$(nmap -sV -A --script=vulscan/vulscan.nse $ip -p $port)" 
+                fi
+            
+       
+    else
+            read -p "[+]Save results? y/n: " save
+            if [ $save = "y" ]
+                    then
+                    read -p  "[+]Location: " savelocation
+                    echo "$(nmap -oN $savelocation -sV -A --script=vulscan/vulscan.nse $ip)" 
+                else
+                    echo "$(nmap -sV -A --script=vulscan/vulscan.nse $ip)" 
                     fi
 fi
 fi
 
 echo "BYE BYE"
-
 
 
